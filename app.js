a68569fa1c0893d043cb8eb401f0105ee94cf40d
Robert Haye <roberthaye1994@gmail.com>
2022-04-21 10:29:05 +0200

    commit 4

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ const app = express();
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
+const _ = require('lodash');
 
 const {
     text
 } = require("body-parser");
+const { response } = require("express");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -29,7 +31,6 @@ const storage = multer.diskStorage({
         callback(null, dir);
     },
     filename: (req, file, callback) => {
-        console.log(file);
         let fullName = Date.now() + path.extname(file.originalname);
         callback(null, fullName)
     }
@@ -75,23 +76,36 @@ app.get("/compose", (req, res) => {
 });
 
 app.post('/compose', upload.array('imageFile', 12), (req, res, next) => {
-    console.log(JSON.stringify(req.file));
+    // console.log(JSON.stringify(req.file));
     for (var i = 0; i < req.files.length; i++) {
-        response = req.files[i].path;
+        var response = req.files[i].path;
     }
-
-    // return res.send(response);
     const post = {
 
         userTitle: req.body.userTitle,
         userImage: response,
         userPost: req.body.userPost
     };
-    console.log(req.body.userPost);
     posts.push(post);
     res.redirect("/")
 });
 
+app.get("/posts/:postName", (req, res) => {
+    const requestedTitle = _.lowerCase(req.params.postName);
+    //Gives access to each indivdual post inside post array:
+    posts.forEach((post) => {
+        const storedTitle = _.lowerCase(post.userTitle);
+        if (storedTitle === requestedTitle) {
+            
+            res.render("post", {
+                title: post.userTitle,
+                image: post.userImage,
+                content: post.userPost
+            });
+        }
+    })
+
+});
 
 app.listen(3000, function () {
     console.log("Server started on port 3000");
